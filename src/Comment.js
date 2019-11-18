ba8075738d4cf601b85e72faf4999c51d5d92e25
Damini varu <daminivaru@Daminis-MacBook-Pro.local>
2019-11-18 16:53:53 -0600

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,20 @@
 //your code here
+import React, { Component } from 'react'; //import React, { Component } from 'react' at the top of our file
+import BlogPost from './BlogPost.js' //import the class in BlogPost
+
+
+export default class Comment extends Component {   //Use the class X extends Component {} syntax
+    render (props) {
+        return (
+            <div className="comment">  
+            {this.props.commentText}
+            </div>
+        )
+    }
+}
+// export default App;  //export the class so it can be used in other files
+
+// The <div> should have a className="comment" attribute
+
+//Don't forget - we can unpack variable values directly with JSX by wrapping them in {}, i.e. {this.props.commentText}
+
